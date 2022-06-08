60cd1bae4f4d844384a35fb88994553a74c09ec2
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-06-08 01:34:27 -0500

    Test and code button click

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test("renders learn react link", () => {
+//   throw new Error("test failed");
+// });
+
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByRole("link", { name: "Learn React" });
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+//expect(element.textContent).toBe("Hello");
+//expect(elementsArray).toHaveLength(7);
+
+//Los tests se separan por commit!
+
+//----------------------------------------
+//What does RTL Do?
+//1. Creates virtual DOM for testing and utilities for interacting with DOM.
+//2. Alloes testing without a browser.
+
+//Types of TESTS
+//1. Unit tests: Tests one unit of CODE in isolation (Functions, components, etc.)
+//2. Integration tests: How multiple units work together.
+//3. Function tests: Tests behavior (NOT CODE) of software function. Ex: Submmit form adata.
+//4. Acceptance / End to end tests: Use actual browser and server (Cypress, Selenium).
+
+//getByRole is better than getByText
+
+//-----------------------------------------SECTION 2-----------------------------------------
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", { name: "Change to blue" }); //Si dejo el role vacío, testing-library me dara sugerencias por consola!!!
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  //Click button
+  fireEvent.click(colorButton);
+
+  //expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  //expect the button text to be "Change to red"
+  expect(colorButton.textContent).toBe("Change to red");
 });
