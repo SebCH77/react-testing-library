a6e0253e671dbbe8a1e6300d34475b9562dd835e
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-06-08 02:01:34 -0500

    Add label to button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -61,7 +61,7 @@ test("initial conditions", () => {
 test("first quiz", () => {
   render(<App />);
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
-  const checkbox = screen.getByRole("checkbox");
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
 
   fireEvent.click(checkbox);
   expect(colorButton).toBeDisabled();
