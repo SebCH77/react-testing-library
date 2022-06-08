113dc57b8fdb693d4b46c1e2954b74143e420c7b
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-06-08 01:56:27 -0500

    Test and code checkbox functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setdisabled] = useState(false);
 
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }} onClick={() => setButtonColor(newButtonColor)}>
+      <button
+        style={{ backgroundColor: buttonColor, color: "white" }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
         Change to {newButtonColor}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setdisabled(e.target.checked)}
+      />
     </div>
   );
 }
